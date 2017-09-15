7e8a47768889f49ff4f95a27e062b13089f8de4b
Jonathon Broughton <jonathon@stardotbmp.com>
2017-09-15 16:53:10 +0100

    Whitespace

--- functions/httpErrors.js
+++ functions/httpErrors.js
@@ -5,13 +5,10 @@ exports.NOT_ALLOWED = (req, res) => {
 
 exports.NOT_IMPLEMENTED = (req, res) => {
     const message = res.locals.errorMessage || "Not Implemented.";
-
     res.status(501).send({ "message": message });
 };
 
 exports.NOT_FOUND = (req, res) => {
-
     const message = res.locals.errorMessage || "Not Found.";
-
     res.status(404).send({ "message": message });
 };
