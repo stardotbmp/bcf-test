38dddc60d7e3779132e65290453c5657eb70cc7b
Jonathon Broughton <jonathon@stardotbmp.com>
2017-09-19 13:35:30 +0100

    rationalised listeners and included data cleanup.

--- functions/triggers/onComment.js
+++ functions/triggers/onComment.js
@@ -2,150 +2,92 @@ const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 const ifcGuid = require("../ifcGuid");
 
-//admin.initializeApp(functions.config().firebase);
+exports.event = functions.database.ref('/import/from_jira/comment/{event}/{id}').onCreate((event) => {
 
-exports.created = functions.database.ref('/import/from_jira/issue_commented/{pushId}')
-    .onCreate((event) => {
-        if (event.data.exists()) {
-            const data = event.data.val();
-            const newComment = data.body.comment;
-            const topicUpdate = data.body.issue;
-            const commentId = new ifcGuid.uuid(newComment.id);
-            const topicId = new ifcGuid.uuid(topicUpdate.id);
-            const issueFields = {};
-            const fields = topicUpdate.fields;
-            let fieldComments;
+    if (event.data.exists()) {
 
-            const dataComment = {
-                author: newComment.author.key,
-                modified_author: newComment.updateAuthor.key,
-                comment: newComment.body,
-                date: newComment.created,
-                modified_date: newComment.updated,
-                topic_guid: topicId.uuid
-            };
+        console.log('onComment');
 
-            issueFields.assigned_to = fields.assignee.key;
-            issueFields.creation_date = fields.created;
-            issueFields.creation_author = fields.reporter.key;
-            issueFields.topic_type = fields.issuetype.name;
-            issueFields.priority = fields.priority.name;
-            issueFields.project_id = (new ifcGuid.uuid(fields.project.id)).uuid;
-            issueFields.topic_status = fields.status.name;
-            issueFields.title = fields.summary;
-            issueFields.modified_date = fields.updated;
-            issueFields.referenceLinks = ['https://grfnconsulting.atlassian.net/browse/' + fields.key];
+        switch (event.params.event) {
+            case 'comment_updated':
+                comment_updated(event);
+                break;
+            case 'comment_deleted':
+                comment_deleted(event);
+                break;
+            default:
+                console.log('Event handler not implemented for ' + event.params.event);
 
-            for (const f in issueFields)
-                if (typeof issueFields[f] === 'undefined')
-                    delete issueFields[f];
-
-            if (topicUpdate.fields.hasOwnProperty('comment')) {
-                fieldComments = topicUpdate.fields.comment.comments.reduce((o, c) => {
-                    var u = new ifcGuid.uuid(c.id);
-                    o[u.uuid] = true;
-                    return o;
-                }, {});
-            }
-
-            const commentReference = admin.database()
-                .ref('data/comments')
-                .child(commentId.uuid);
-
-            const topicReference = admin.database()
-                .ref('data/topics')
-                .child(topicId.uuid);
-
-            const topicCommentReference = topicReference
-                .child('comments');
-
-            commentReference.set(dataComment)
-                .then(() => {
-                    topicReference.update(issueFields)
-                        .then(() => {
-                            topicCommentReference.update(fieldComments)
-                                .then(() => {
-                                    event.data.ref.remove();
-                                }).catch(() => {
-                                    console.log('topicComments not updated');
-                                });
-                        })
-                        .catch(() => {
-                            console.log('topic not updated');
-                        });
-                }).catch(() => {
-                    console.log('comment not added');
-                });
+                // unhandled data reference - delete.
+                event.data.ref.remove();
+                break;
         }
-    });
+    }
+    return;
+});
 
-exports.updated = functions.database.ref('/import/from_jira/comment_updated/{pushId}')
-    .onCreate((event) => {
+const comment_updated = function(event) {
+    const data = event.data.val();
+    const updatedComment = data.body.comment;
+    const topic = updatedComment.self.match(/issue\/(\d+)\/comment/)[1];
+    const topicId = new ifcGuid.uuid(topic);
+    const commentId = new ifcGuid.uuid(updatedComment.id);
 
-        if (event.data.exists()) {
-            const data = event.data.val();
-            const updatedComment = data.body.comment;
-            const topic = updatedComment.self.match(/issue\/(\d+)\/comment/)[1];
-            const topicId = new ifcGuid.uuid(topic);
-            const commentId = new ifcGuid.uuid(updatedComment.id);
+    const dataComment = {
+        author: updatedComment.author.key,
+        modified_author: updatedComment.updateAuthor.key,
+        comment: updatedComment.body,
+        date: updatedComment.created,
+        modified_date: updatedComment.updated,
+        topic_guid: topicId.uuid
+    };
 
-            const dataComment = {
-                author: updatedComment.author.key,
-                modified_author: updatedComment.updateAuthor.key,
-                comment: updatedComment.body,
-                date: updatedComment.created,
-                modified_date: updatedComment.updated,
-                topic_guid: topicId.uuid
-            };
+    const commentReference = admin.database()
+        .ref('data/comments')
+        .child(commentId.uuid);
 
-            const commentReference = admin.database()
-                .ref('data/comments')
-                .child(commentId.uuid);
+    const topicReference = admin.database()
+        .ref('data/topics')
+        .child(topicId.uuid);
 
-            const topicReference = admin.database()
-                .ref('data/topics')
-                .child(topicId.uuid);
-
-            commentReference.update(dataComment)
+    commentReference.update(dataComment)
+        .then(() => {
+            console.log('comment updated.');
+            topicReference.update({ modified_date: updatedComment.updated })
                 .then(() => {
-                    console.log('comment updated.');
-                    topicReference.update({ modified_date: updatedComment.updated })
-                        .then(() => {
-                            event.data.ref.remove();
-                        })
-                        .catch((error) => {
-                            console.log('issue modified date not updated.');
-                        });
+                    event.data.ref.remove();
                 })
                 .catch((error) => {
-                    console.log('comment not updated.');
+                    console.log('issue modified date not updated.');
                 });
-        }
-    });
+        })
+        .catch((error) => {
+            console.log('comment not updated.');
+        });
+};
 
-exports.deleted = functions.database.ref('/import/from_jira/comment_deleted/{pushId}')
-    .onCreate((event) => {
-        if (event.data.exists()) {
-            const data = event.data.val();
-            const deletedComment = data.body.comment;
-            const commentId = new ifcGuid.uuid(deletedComment.id);
-            const topic = deletedComment.self.match(/issue\/(\d+)\/comment/)[1];
-            const topicId = new ifcGuid.uuid(topic);
+const comment_deleted = function(event) {
+    if (event.data.exists()) {
+        const data = event.data.val();
+        const deletedComment = data.body.comment;
+        const commentId = new ifcGuid.uuid(deletedComment.id);
+        const topic = deletedComment.self.match(/issue\/(\d+)\/comment/)[1];
+        const topicId = new ifcGuid.uuid(topic);
 
-            const commentReference = admin.database()
-                .ref('data/comments')
-                .child(commentId.uuid);
+        const commentReference = admin.database()
+            .ref('data/comments')
+            .child(commentId.uuid);
 
-            const topicCommentReference = admin.database()
-                .ref('data/topics')
-                .child(topicId.uuid)
-                .child('comments')
-                .child(commentId.uuid);
+        const topicCommentReference = admin.database()
+            .ref('data/topics')
+            .child(topicId.uuid)
+            .child('comments')
+            .child(commentId.uuid);
 
-            commentReference.remove();
-            topicCommentReference.remove();
-            event.data.ref.remove();
-        }
+        commentReference.remove();
+        topicCommentReference.remove();
+        event.data.ref.remove();
+    }
 
-        return;
-    });
+    return;
+};
---
+++ functions/triggers/onIssue.js
@@ -0,0 +1,164 @@
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const ifcGuid = require("../ifcGuid");
+
+exports.event = functions.database.ref('/import/from_jira/issue/{event}/{id}').onCreate((event) => {
+
+    if (event.data.exists()) {
+
+        console.log('onIssue');
+
+        switch (event.params.event) {
+            case 'issue_updated':
+                issue_updated(event);
+                break;
+            case 'issue_created':
+                issue_updated(event);
+                break;
+            case 'issue_generic':
+                issue_updated(event);
+                break;
+            case 'issue_commented':
+                issue_commented(event);
+                break;
+            case 'issue_assigned':
+                issue_updated(event);
+                break;
+            default:
+                console.log('Event handler not implemented for ' + event.params.event);
+
+                // unhandled data reference - delete.
+                event.data.ref.remove();
+                break;
+        }
+    }
+    return;
+});
+
+const issue_updated = function(event) {
+    const data = event.data.val();
+    const topicUpdate = data.body.issue;
+    const topicUpdateUser = data.body.user;
+    const topicFields = topicUpdate.fields;
+    const topicChanges = data.body.changelog;
+    const topicId = new ifcGuid.uuid(topicUpdate.id);
+    const fields = topicUpdate.fields;
+    const projectUpdate = topicUpdate.fields.project;
+    const projectId = new ifcGuid.uuid(projectUpdate.id);
+    const issueFields = {};
+
+    issueFields.assigned_to = definedAttribute(fields.assignee, 'key');
+    issueFields.creation_date = fields.created;
+    issueFields.creation_author = definedAttribute(fields.reporter, 'key');
+    issueFields.topic_type = definedAttribute(fields.issuetype, 'name');
+    issueFields.description = fields.description;
+    issueFields.priority = definedAttribute(fields.priority, 'name');
+    issueFields.project_id = projectId.uuid;
+    issueFields.topic_status = definedAttribute(fields.status, 'name');
+    issueFields.title = fields.summary;
+    issueFields.modified_date = fields.updated;
+    issueFields.modified_user = topicUpdateUser.key;
+
+    if (definedAttribute(fields.key)) {
+        issueFields.referenceLinks = ['https://grfnconsulting.atlassian.net/browse/' +
+            fields.key
+        ];
+    }
+
+    let labels = (fields.labels || []).reduce((labels, label) => labels[label] = true, {});
+
+    for (const f in issueFields)
+        if (typeof issueFields[f] === 'undefined')
+            delete issueFields[f];
+
+    const topicReference = admin.database()
+        .ref('data/topics')
+        .child(topicId.uuid);
+
+    const projectReference = admin.database()
+        .ref('data/projects')
+        .child(projectId.uuid);
+
+        const labelsReference = admin.database()
+        .ref('data/topics/')
+
+    topicReference
+        .update(issueFields)
+        .then(() => {
+            console.log('topic updated.');
+            projectReference
+                .update({
+                    name: projectUpdate.name,
+                    jira_key: projectUpdate.key
+                })
+                .then(() => {
+                    console.log('project updated.');
+                    event.data.ref.remove();
+                }).catch((error) => {
+                    console.log('project not updated', error);
+                });
+        })
+        .catch((error) => {
+            console.log('topic not updated', error);
+            console.log(issueFields);
+        });
+};
+
+const definedAttribute = function(attribute, child) {
+    if (attribute === undefined) { return; }
+    if (child === undefined) { return attribute; }
+    if (child && attribute[child] === undefined) { return; }
+    return attribute[child];
+};
+
+const issue_commented = function(event) {
+    if (event.data.exists()) {
+        const data = event.data.val();
+        const newComment = data.body.comment;
+        const topicUpdate = data.body.issue;
+        const commentId = new ifcGuid.uuid(newComment.id);
+        const topicId = new ifcGuid.uuid(topicUpdate.id);
+        const fields = topicUpdate.fields;
+        let fieldComments;
+
+        const dataComment = {
+            author: newComment.author.key,
+            modified_author: newComment.updateAuthor.key,
+            comment: newComment.body,
+            date: newComment.created,
+            modified_date: newComment.updated,
+            topic_guid: topicId.uuid
+        };
+
+        if (topicUpdate.fields.hasOwnProperty('comment')) {
+            fieldComments = topicUpdate.fields.comment.comments.reduce((o, c) => {
+                var u = new ifcGuid.uuid(c.id);
+                o[u.uuid] = true;
+                return o;
+            }, {});
+        }
+
+        const commentReference = admin.database()
+            .ref('data/comments')
+            .child(commentId.uuid);
+
+        const topicCommentReference = admin.database()
+            .ref('data/topics')
+            .child(topicId.uuid)
+            .child('comments');
+
+        commentReference
+            .set(dataComment)
+            .then(() => {
+                topicCommentReference
+                    .update(fieldComments)
+                    .then(() => {
+                        event.data.ref.remove();
+                    }).catch((error) => {
+                        console.log('topicComments not updated', error);
+                    });
+            }).catch((error) => {
+                console.log('comment not added', error);
+            });
+    }
+};
